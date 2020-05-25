39c2cf46c19c9ec0f52ff2f25657a2ffd6e18070
EstebanMagallonPerez <estebanmagallonperez@gmail.com>
2020-05-24 23:30:35 -0700

    Refactor code a bit

--- WebServer.py
+++ WebServer.py
@@ -1,13 +1,12 @@
 from aiohttp import web
 import socketio
 import asyncio
-
-#from pynput.keyboard import Key, Listener
-
 import keyboard
 import threading
+import os
 
 def start():
+	sio = socketio.AsyncServer()
 	recorded = []
 	def print_pressed_keys(e):
 		if e.event_type == "down":
@@ -26,19 +25,8 @@ def start():
 		keyboard.hook(print_pressed_keys)
 		keyboard.wait()
 
-	watcherThread = threading.Thread(target=keyboardWatcher)
-	watcherThread.start()
-
-
-
-
-	sio = socketio.AsyncServer()
-	app = web.Application()
-	sio.attach(app)
-
 	async def index(request):
 		fileName = request.path
-		print(request.path)
 		if fileName == "/":
 			fileName = "/index.html"
 		contentType = ""
@@ -52,34 +40,25 @@ def start():
 		with open('.'+fileName) as f:
 			return web.Response(text=f.read(), content_type=contentType)
 
-	app.router.add_get('/', index)
-	app.router.add_get('/generateKeyboard.html', index)
-	app.router.add_get('/keyboard.json', index)
-	app.router.add_get('/keyboardFull.json', index)
-	app.router.add_get('/style.css', index)
-
 	async def print_message(messageType, message):
 		task = asyncio.create_task(
 			sio.emit(messageType, message))
 		await task
-		
-
 
 	@sio.on('message')
 	async def received_message(sid, message):
-		#This is always sending and receiving because there is some issue where the key presses get frozen and htis allows me to push them through
 		await sio.emit('message', message[::-1])
 
-	#def on_press(key):
-	#	asyncio.run(print_message("press",str(key)))
-		
-	#def on_release(key):
-	#	asyncio.run(print_message("release",str(key)))
+	watcherThread = threading.Thread(target=keyboardWatcher)
+	watcherThread.start()
 
-	#listener = Listener(
-	#	on_press=on_press,
-	#	on_release=on_release)
-	#listener.start()
+	app = web.Application()
+	sio.attach(app)
 
+	app.router.add_get('/', index)
+	app.router.add_get('/generateKeyboard.html', index)
+	for file in os.listdir("./"):
+		if file.endswith(".json"):
+			app.router.add_get('/'+file, index)
 	web.run_app(app)
 start()
